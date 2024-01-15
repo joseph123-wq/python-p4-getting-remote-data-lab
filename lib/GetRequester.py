a5deea8632095738abd9e1283c469de470bec289
JosephKagwi <josephkagwi28@gmail.com>
2024-01-15 12:11:07 +0300

    assignment complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,15 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        try:
+            json_data = json.loads(response_body)
+            return json_data
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            return None
+
